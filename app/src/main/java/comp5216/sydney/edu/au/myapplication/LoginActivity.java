1a09db9973d35722ac2b93cdf70ba477d4ee0da1
Bowen-Sun <53130446+Bowen-Sun@users.noreply.github.com>
2020-10-30 09:30:33 +0800

    Revert "update"
    This reverts commit 0218d0369b47b073dde431066033e1bfe5bfb664.

--- app/src/main/java/comp5216/sydney/edu/au/myapplication/LoginActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/myapplication/LoginActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,8 +20,7 @@ public class LoginActivity extends AppCompatActivity {
 
 
     EditText id,password;
-    Button login;
-    TextView signup;
+    Button login,signup;
     FirebaseAuth mAuth;
 
 
