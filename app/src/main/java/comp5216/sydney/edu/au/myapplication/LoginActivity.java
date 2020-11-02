631acd3013a1c08c15789665c5c4701f9d26cb44
lau <765712426@qq.com>
2020-11-02 17:48:04 +1100

    login ui

--- app/src/main/java/comp5216/sydney/edu/au/myapplication/LoginActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/myapplication/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,7 +21,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
     EditText id,password;
-    Button login,signup;
+    Button login;
+    TextView signup;
     FirebaseAuth mAuth;
 
 
